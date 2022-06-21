a152759a65ac1f5ef1b05124e01e5a96984dc8fe
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-21 14:11:33 +0300

    .

--- src/Grocery.java
+++ src/Grocery.java
@@ -1,13 +1,19 @@
+/* Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+Meyveler ve KG Fiyatları = Armut : 2,14 TL , Elma : 3,67 TL , Domates : 1,11 TL , Muz: 0,95 TL , Patlıcan : 5,00 TL */
+
 import java.util.Scanner;
 
 public class Grocery {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
         double a = 2.14, e = 3.67, d = 1.11, m = 0.95, p = 5.0;
         int ar, el, dom, mu, pa;
         double toplam;
 
-        Scanner input = new Scanner(System.in);
+        // Kullanıcıdan girişleri aldıktan sonra formül yazıp toplamı buluyoruz.
+
+        System.out.println();
         System.out.print("Kac Kilo Armut Aldiniz? : ");
         ar = input.nextInt();
         System.out.print("Kac Kilo Elma Aldiniz? : ");
@@ -18,8 +24,9 @@ public class Grocery {
         mu = input.nextInt();
         System.out.print("Kac Kilo Patlican Aldiniz? : ");
         pa = input.nextInt();
+        System.out.println();
 
         toplam = ((a * ar) + (e * el) + (d * dom) + (m * mu) + (p * pa));
-        System.out.print("Toplam Odeyeceginiz Tutar : " + toplam + "TL");
+        System.out.println("Toplam Odeyeceginiz Tutar : " + toplam + "TL");
     }
 }
